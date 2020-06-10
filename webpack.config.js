30a987c17e07ccb3d8ac5db87871d7f6f8ac1890
Ludde86 <bslbjorn@gmail.com>
2020-06-10 14:17:20 +0200

    setup react-router

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+// entry poiont -> where our application kick of (app.js)
+// output the final bundle file (javascript file that contains everything our app needs to run)
+
+// the absolute path to our apllication
+// join together two paths -> path to our app + public folder
+module.exports = {
+	entry: './src/app.js',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			},
+			{
+				test: /\.s?css$/,
+				use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+			}
+		]
+	},
+	devtool: 'cheap-module-eval-source-map',
+	devServer: {
+		contentBase: path.join(__dirname, 'public'),
+		historyApiFallback: true
+	}
+};
+
+// loader - customize the behavior of webpack, when it loads a given file
+// setup the loader (from babel)
